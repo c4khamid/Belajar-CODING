796dcbfe28d553a78e393c2a2c3e54171e878ff2
c4khamid <c4khamid@gmail.com>
2021-09-10 21:08:29 +0700

    Belajar Pertama

---
+++ tugas 1.5.1.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main ()
+{
+	// clrscr();
+	system("cls");
+	
+	printf("\t\tPRAKTIKUM\n");
+	printf("\t    PEMROGRAMAN BAHASA C\n");
+	printf("POLITEKNIK ELEKTRONIKA NEGERI SURABAYA\n");
+	printf("      INSTITUT TEKNOLOGI SEPULUH NOPEMBER\n");
+	printf("\t\tSURABAYA\n");
+}
